0c8c4e3869b653cd7934230f5c64e1e86c670a27
dolan1g <senad.topalovic@hotmail.com>
2017-04-18 11:16:33 +0200

    Add files via upload

---
+++ BGLooper.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGLooper : MonoBehaviour
+{
+
+    int numBGPanels = 6;
+    float CijeviMax = 3.10f;
+    float CijeviMin = 2.25f;
+
+
+
+    void Start()
+    {
+        GameObject[] cijevi = GameObject.FindGameObjectsWithTag("Cijevi");
+        foreach (GameObject cijev in cijevi)
+        {
+            Vector3 pozicija = cijev.transform.position;
+            pozicija.y = Random.Range(CijeviMin, CijeviMax);
+            cijev.transform.position = pozicija;
+        }
+
+    }
+
+    
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Triggered: " + collider.name);
+
+        float SirinaBGOBjekta = ((BoxCollider2D)collider).size.x;
+        Vector3 pozicija = collider.transform.position;
+        pozicija.x += SirinaBGOBjekta * numBGPanels;
+        if(collider.tag=="Cijevi")
+        {
+            pozicija.y = Random.Range(CijeviMin, CijeviMax);
+        }
+        collider.transform.position = pozicija;
+
+    }
+}
---
+++ KameraPratiIgraca.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KameraPratiIgraca : MonoBehaviour {
+    Transform igrac;
+    float offsetX;
+	// Use this for initialization
+	void Start () {
+
+        GameObject igrac_go = GameObject.FindGameObjectWithTag("Player");
+        if(igrac_go==null)
+        {
+            Debug.LogError("Nije se mogao pronaci objekat sa tagom Player");
+            return;
+        }
+        igrac = igrac_go.transform;
+        offsetX = transform.position.x - igrac.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (igrac != null)
+        {
+            Vector3 pozicija = transform.position;
+            pozicija.x = igrac.position.x + offsetX;
+            transform.position = pozicija;
+        }
+        
+	
+	}
+}
